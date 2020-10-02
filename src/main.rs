3709e1d49b4904d78b672db5df077f1c01451977
Glen Sargent <glensarg@hotmail.co.uk>
2020-10-02 17:15:17 +0100

    start implementing vector ops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+#[derive(Debug, Clone, PartialEq)]
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+impl Vec2 {
+    fn new(x: f64, y: f64) -> Self {
+        Vec2{x, y}
+    }
+
+    fn mangitude(&self) -> f64 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y
+        }
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y
+        }
+    }
+}
+
+impl Div for Vec2 {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self {
+        Self {
+            x: self.x / other.x,
+            y: self.y / other.y
+        }
+    }
+}
+
+impl Mul for Vec2 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test = Vec2::new(10.0, 20.0);
+    let test2 = Vec2::new(50.0, 20.0);
+    // let res = test + test2;
+    println!("{}", test2.mangitude());
+
+    // assert_eq!(res, Vec2{x: 60.0, y: 40.0});
 }
